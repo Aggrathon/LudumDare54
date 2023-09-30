4678d674ccef68b9641ce0d3f44262635b48005a
Aggrathon <antonbjo@gmail.com>
2023-09-30 14:44:05 +0300

    camera movement

---
+++ src/camera.rs
@@ -0,0 +1,110 @@
+use std::f32::consts::PI;
+use std::time::Duration;
+
+use bevy::prelude::*;
+use bevy_easings::*;
+
+pub struct CameraMovePlugin;
+
+impl Plugin for CameraMovePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup)
+            // .add_systems(Update, animate_camera_direction)
+            .add_systems(PreUpdate, camera_rot)
+            .add_systems(PostUpdate, camera_move);
+    }
+}
+
+#[derive(Component)]
+struct CameraDolly;
+
+#[derive(Component)]
+struct CameraArm;
+
+fn setup(mut commands: Commands) {
+    let rot = Quat::from_rotation_y(PI * 0.25);
+    commands
+        .spawn((
+            CameraDolly,
+            TransformBundle::from_transform(Transform::from_rotation(rot)),
+        ))
+        .with_children(|p| {
+            p.spawn((CameraArm, TransformBundle { ..default() }))
+                .with_children(|p| {
+                    p.spawn(Camera3dBundle {
+                        transform: Transform::from_xyz(10.0, 10.0, 0.0)
+                            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+                        ..default()
+                    });
+                });
+        });
+}
+
+fn camera_rot(
+    query: Query<(&Transform, Entity), With<CameraArm>>,
+    keys: Res<Input<KeyCode>>,
+    mut commands: Commands,
+) {
+    for (camera, entity) in query.iter() {
+        if keys.just_pressed(KeyCode::E) {
+            let mut rot = camera.rotation.to_euler(EulerRot::YXZ).0;
+            rot = ((rot / (PI * 0.5) + 0.1).floor() + 1.0) * (PI * 0.5);
+            commands.entity(entity).insert(camera.ease_to(
+                camera.with_rotation(Quat::from_rotation_y(rot)),
+                EaseFunction::QuadraticInOut,
+                EasingType::Once {
+                    duration: Duration::from_millis(500),
+                },
+            ));
+        }
+        if keys.just_pressed(KeyCode::Q) {
+            let mut rot = camera.rotation.to_euler(EulerRot::YXZ).0;
+            rot = ((rot / (PI * 0.5) - 0.1).ceil() - 1.0) * (PI * 0.5);
+            commands.entity(entity).insert(camera.ease_to(
+                camera.with_rotation(Quat::from_rotation_y(rot)),
+                EaseFunction::QuadraticInOut,
+                EasingType::Once {
+                    duration: Duration::from_millis(500),
+                },
+            ));
+        }
+    }
+}
+
+fn camera_move(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<CameraDolly>>,
+    camera: Query<&GlobalTransform, With<Camera3d>>,
+    keys: Res<Input<KeyCode>>,
+) {
+    let camera = camera.single();
+    for mut transform in query.iter_mut() {
+        let mut input = Vec3::default();
+        if keys.pressed(KeyCode::W) || keys.pressed(KeyCode::Up) {
+            input.z -= 1.0;
+        }
+        if keys.pressed(KeyCode::S) || keys.pressed(KeyCode::Down) {
+            input.z += 1.0;
+        }
+        if keys.pressed(KeyCode::A) || keys.pressed(KeyCode::Left) {
+            input.x -= 1.0;
+        }
+        if keys.pressed(KeyCode::D) || keys.pressed(KeyCode::Right) {
+            input.x += 1.0;
+        }
+        let rot = camera
+            .to_scale_rotation_translation()
+            .1
+            .to_euler(EulerRot::YXZ)
+            .0;
+        transform.translation +=
+            Quat::from_rotation_y(rot) * (input.normalize_or_zero() * time.delta_seconds() * 5.0);
+    }
+}
+
+#[allow(dead_code)]
+fn animate_camera_direction(time: Res<Time>, mut query: Query<&mut Transform, With<CameraArm>>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() * PI / 10.0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use bevy::prelude::*;
-use level::{LevelPlugin, LoadLevel};
 use std::f32::consts::*;
 
+use bevy::prelude::*;
+use bevy_easings::EasingsPlugin;
+
+use camera::CameraMovePlugin;
+use level::{LevelPlugin, LoadLevel};
+
+mod camera;
 mod level;
 
 fn main() {
@@ -11,10 +16,9 @@ fn main() {
             brightness: 1.0 / 8.0f32,
         })
         .add_state::<AppState>()
-        .add_plugins((DefaultPlugins, LevelPlugin))
+        .add_plugins((DefaultPlugins, LevelPlugin, CameraMovePlugin, EasingsPlugin))
         .add_systems(Startup, setup)
         .add_systems(Update, animate_light_direction)
-        .add_systems(Update, animate_camera_direction)
         .run();
 }
 
@@ -32,10 +36,6 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-        ..default()
-    });
     commands.spawn(DirectionalLightBundle {
         transform: Transform::from_xyz(0.0, 3.0, 0.0).looking_at(Vec3::new(1.0, 0.0, 1.0), Vec3::Y),
         directional_light: DirectionalLight {
@@ -61,16 +61,5 @@ fn animate_light_direction(
             ),
             Vec3::Y,
         );
-    }
-}
-
-fn animate_camera_direction(time: Res<Time>, mut query: Query<&mut Transform, With<Camera3d>>) {
-    for mut transform in &mut query {
-        *transform = Transform::from_xyz(
-            f32::sin(time.elapsed_seconds() * PI / 10.0) * 10.0,
-            10.0,
-            f32::cos(time.elapsed_seconds() * PI / 10.0) * 10.0,
-        )
-        .looking_at(Vec3::ZERO, Vec3::Y);
     }
 }
